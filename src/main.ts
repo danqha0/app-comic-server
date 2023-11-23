7a687b8e42fb094a253a6992fa2139beb9e03bab
Dang Hao <62649837+haotamht@users.noreply.github.com>
2023-11-23 19:31:52 +0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+//import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  //app.use(csurf());
+
   app.enableCors();
   await app.listen(2001);
 }
