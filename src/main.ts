13b654c69f8cc3d3cc75c9e1b3296f88bc55ee80
Dang Hao <danghao1209@gmail.com>
2024-12-26 23:25:34 +0700

    finish

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,29 @@ import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 //import * as csurf from 'csurf';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.use(helmet());
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Cats example')
+//     .setDescription('The cats API description')
+//     .setVersion('1.0')
+//     .addTag('cats')
+//     .build();
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   app.enableCors();
+//   await app.listen(2001);
+// }
+// bootstrap();
+
+import { AppClusterService } from './cluster.service';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet());
-
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  app.enableCors();
-  await app.listen(2001);
+  await app.listen(3000);
 }
-bootstrap();
+AppClusterService.clusterize(bootstrap);
